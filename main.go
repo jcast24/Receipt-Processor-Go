5de32ee5de8eb43cdc52188c0aba4ad4b213da9a
Jesus Castro <castrojesus39@gmail.com>
2024-12-12 23:22:29 -0800

    finished completion of CheckTotal and CheckItemsCount, still figuring out how to implement CheckDescription

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"strconv"
+	"strings"
 	"unicode"
 
 	"github.com/google/uuid"
@@ -40,6 +43,65 @@ func CheckAlphanumeric(r *Receipt) int {
     return count
 }
 
+func CheckTotal(r *Receipt) int {
+    total := r.Total
+
+    floatValue, err := strconv.ParseFloat(total, 64)
+    if err != nil {
+        fmt.Println("Error: ", err)
+    }
+
+    isWholeNumber := math.Mod(floatValue, 1) == 0
+    isMultipleOfQuarter :=  math.Mod(floatValue, 0.25) == 0
+
+    if (isWholeNumber && isMultipleOfQuarter) {
+        return 75
+    } else if (isWholeNumber) {
+        return 50;
+    } else if (isMultipleOfQuarter) {
+        return 25;
+    }
+    return 0
+}
+
+func CheckItemsCount(r *Receipt) int {
+    count := (len(r.Items) / 2) * 5
+    return count
+}
+
+func CheckDescription(r *Receipt) int {
+    var result = 0.0
+    var final = 0
+    var roundedResult = 0.0
+    var roundedResultUp = 0
+
+    for _,values := range r.Items {
+        description := values.ShortDescription 
+        trimDescription := strings.Trim(description, " ")
+        
+
+        price := values.Price
+        
+        // convert price from string to float
+        floatPrice, err := strconv.ParseFloat(price, 64)
+        if err != nil {
+            fmt.Println("Error: ", err)
+        }
+    
+        var multiplier = 0.25
+
+        if (math.Mod(len(trimDescription), 3) == 0) {
+            result = floatPrice * multiplier
+            roundedResult = math.Round(result)
+        }
+
+    }
+
+
+    return 0
+}
+
+
 func CreateIdForReceipt(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var newReceipt Receipt
@@ -68,6 +130,7 @@ func CreateIdForReceipt(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
 func GetPointsById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -93,7 +156,10 @@ func GetPointsById(w http.ResponseWriter, r *http.Request) {
     // }
 
 	checkNameResult := CheckAlphanumeric(foundReceipt)
-	finalPoints := checkNameResult
+    checkTotalResult := CheckTotal(foundReceipt)
+    checkItemsResult := CheckTotal(foundReceipt)
+
+	finalPoints := checkNameResult + checkTotalResult + checkItemsResult
 
 	response := struct {
 		Points int `json:"points"`
